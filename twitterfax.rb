f1b07783869f5d6a3ec217875db46e7da6207961
Martin Ström <name@my-domain.se>
2010-05-27 03:21:35 -0700

    improved layout + support yfrog and twitpic

--- twitterfax.rb
+++ twitterfax.rb
@@ -1,24 +1,49 @@
-%w[rubygems twitter json prawn tempfile].each { |l| require l }
+%w[rubygems twitter json prawn tempfile open-uri].each { |l| require l }
 
-USERNAME     = 'screenname'
-PASSWORD     = 'password'
-DATA_FILE    = 'twitterfax.json'
+USERNAME     = 'username'
+PASSWORD     = 'p4ssw0rd'
+DATA_FILE    = File.dirname(__FILE__) + "/twitterfax.json"
 PRINTER_NAME = 'HP_LaserJet_2420' # `lpstat -a`
 
 data_string  = File.read(DATA_FILE) rescue "{}"
 data         = JSON.parse(data_string)
 http_auth    = Twitter::HTTPAuth.new(USERNAME, PASSWORD)
 base         = Twitter::Base.new(http_auth)
-mentions     = base.mentions#(data)
+mentions     = base.mentions(data)
+
+IMAGE_SERVICES = {
+  %r{(\s*http://yfrog.com/(\w+)\s*)} => "http://yfrog.com/%s.th.jpg",
+  %r{(\s*http://twitpic.com/(\w+)\s*)} => "http://twitpic.com/show/thumb/%s",
+}
 
 mentions.each do |tweet|
   temp = Tempfile.new('print_tweet_')
   time = Time.parse(tweet.created_at).strftime('%H:%M %B %d %Y')
   
+  images = []
+  
   Prawn::Document.generate(temp.path, :margin => 59, :left_margin => 107, :right_margin => 55) do |pdf|
-    # pdf.font 'LucidFaxEFRom.ttf'
-    pdf.text "#{tweet.text}\n\nBy @#{tweet.user['screen_name']}\n#{time}", 
-      :size => 42, :leading => 10, :kerning => true
+    text = tweet.text
+
+    IMAGE_SERVICES.each do |pattern, embed_url|
+      text.scan(pattern).each do |url, id|
+        images << sprintf(embed_url, id)
+        text.gsub!(url, '')
+      end
+    end
+
+    pdf.font File.dirname(__FILE__) + '/LucidFaxEFRom.ttf'
+    pdf.font_size 42
+    pdf.text text, :leading => 10
+    
+    images.each do |url|
+      pdf.move_down 10
+      pdf.image open(url), :scale => 2
+    end
+    
+    pdf.move_down 20
+    
+    pdf.text "By @#{tweet.user['screen_name']}\n#{time}", :leading => 10
   end
   
   # `open -a Preview #{temp.path}`
